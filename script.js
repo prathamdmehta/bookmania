25229bf5967a3507f14799c85746e8de6dd87cf4
Pratham Mehta <115774050+prathamdmehta@users.noreply.github.com>
2024-09-13 09:30:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -107,3 +107,45 @@ var swiper = new Swiper(".arrivals-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetweeen: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetweeen: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
